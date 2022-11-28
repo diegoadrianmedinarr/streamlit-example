43cc2919591f6660d65fd4aaa52a037e911b687c
Diego Medina <119269689+diegoadrianmedinarr@users.noreply.github.com>
2022-11-27 19:21:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,1 @@
-import streamlit as st
-import pandas as pd
-import base64
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-
 st.title('NFL Football Stats (Rushing) Explorer')
-
-st.markdown("""
-This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)!
-""")
