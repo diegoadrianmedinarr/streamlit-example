767ac8a4e009f4417faab0dd6b34ec3e8c571a89
Diego Medina <119269689+diegoadrianmedinarr@users.noreply.github.com>
2022-11-29 12:03:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import yfinance as yf
 
 st.title('Presupuesto 2023 - León')
+
+st.write("""A continuación se muestra el presupuesto anual del año 2023 para la unidad de negocio de León""")
 
 st.subheader('Gastos por mes')
 
 data = {'Enero': [100], 'Febrero': [80]}
 
 df = pd.DataFrame(data=data)
+
+st.line_chart(data)
+
+
